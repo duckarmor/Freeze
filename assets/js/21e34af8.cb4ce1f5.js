f3872c8f4c5637428eb6772b577cef3d949bd696
benbrimeyer <benjaminbrimeyer@gmail.com>
2023-04-05 14:16:15 -0700

    Built and published by Moonwave

---
+++ assets/js/21e34af8.cb4ce1f5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[497],{7636:s=>{s.exports=[]}}]);
--- assets/js/a5f8ad90.ee05227d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[191],{97868:function(e){e.exports=JSON.parse('{"sourceUrl":"https://github.com/benbrimeyer/Freeze/blob/master","baseUrl":"/Freeze/","classOrder":[],"apiCategories":[]}')}}]);
